dcab4b11759ac91c16ae95ec62dd18f7781f9a10
Karan <karan03945@gmail.com>
2026-06-07 15:20:45 +0530

    feat: cros added for cravingsfood.netlify.app

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ const server = createServer(app);
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://10.53.203.71:5173"],
+    origin: ["http://localhost:5173", "http://10.53.203.71:5173","https://cravingsfood.netlify.app"],
     credentials: true,
   }),
 );
--- server/src/config/socket.js
+++ server/src/config/socket.js
@@ -7,7 +7,7 @@ const userSocketMap = {};
 const initializeSocket = (server) => {
   io = new Server(server, {
     cors: {
-      origin: ["http://localhost:5173"],
+      origin: ["http://localhost:5173", "https://cravingsfood.netlify.app"],
       credentials: true,
     },
   });
