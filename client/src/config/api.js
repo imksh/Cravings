9516307a40cfb750bd487a2517edf6ae52251c28
Karan <karan03945@gmail.com>
2026-06-07 15:57:33 +0530

    dynamic link added

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5001/api",
-  // baseURL: "https://cravings-o6av.onrender.com/api",
+  baseURL: `${import.meta.env.VITE_PUBLIC_API_URL}/api`,
   headers: {
     "x-platform": "web",
   },
--- client/src/config/socket.js
+++ client/src/config/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io(import.meta.env.VITE_BACKEND_URL || "http://localhost:4500", {
+const socket = io(import.meta.env.VITE_PUBLIC_API_URL || "http://localhost:4500", {
   withCredentials: true,
   autoConnect: false,
 });
